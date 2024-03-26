00e4dc7d8affc5e26da234ca9115349512f1f930
Ana A. Ramos <123672228+Ana-Julia16@users.noreply.github.com>
2024-03-26 08:11:06 -0300

    Create script.js

---
+++ JS/ex6/script.js
@@ -0,0 +1,8 @@
+        var count = 0;
+        var btn = document.getElementById("btn");
+        var disp = document.getElementById("idconta");
+  
+        btn.onclick = function () {
+            count++;
+            disp.innerHTML = count;
+        }
